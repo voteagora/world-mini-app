eda1f18c0d9b0992fb10275864a1525620dbc148
Pedro Fernandes <pedrohasftb@gmail.com>
2025-07-03 14:50:13 -0300

    about page

--- src/app/(protected)/about/page.tsx
+++ src/app/(protected)/about/page.tsx
@@ -14,22 +14,33 @@ export default function About() {
         </div>
         <div className="flex flex-col items-start justify-start gap-4 mt-4">
           <Typography variant="body" level={2} color="default">
-            Lorem ipsum dolor sit amet consectetur. Feugiat varius accumsan non
-            pretium posuere urna arcu arcu. Viverra sed eget sodales nibh
-            pulvinar justo volutpat. Tincidunt congue dignissim pellentesque
-            integer sagittis. Nisl semper eu id nulla adipiscing pellentesque
-            sem nibh. Ipsum tempor sagittis volutpat fermentum sit eget nisl
-            sed. Enim eu mauris amet tellus tempor volutpat sagittis. Mi libero
-            commodo vitae et eget tellus. Nibh cursus nibh feugiat netus. Urna
-            egestas amet aliquam faucibus eget magna purus donec. Lorem ipsum
-            dolor sit amet consectetur. Feugiat varius accumsan non pretium
-            posuere urna arcu arcu. Viverra sed eget sodales nibh pulvinar justo
-            volutpat. Tincidunt congue dignissim pellentesque integer sagittis.
-            Nisl semper eu id nulla adipiscing pellentesque sem nibh. Ipsum
-            tempor sagittis volutpat fermentum sit eget nisl sed. Enim eu mauris
-            amet tellus tempor volutpat sagittis. Mi libero commodo vitae et
-            eget tellus. Nibh cursus nibh feugiat netus. Urna egestas amet
-            aliquam faucibus eget magna purus donec.
+            The World Vote Mini App is an experiment in onchain governance
+            developed by World Foundation in partnership with Agora. Founded in
+            2022 to steward the World network, the World Foundation has teamed
+            up with Agora, a leading platform for onchain governance to explore
+            potential ways the World community can vote on operational decisions
+            affecting the network.
+          </Typography>
+          <Typography variant="body" level={2} color="default">
+            World is a network of real humans, built on privacy-preserving
+            proof-of-human technology, and powered by a globally inclusive
+            financial network that enables the free flow of digital assets for
+            all. It is built to connect, empower, and be owned by everyone.
+          </Typography>
+          <Typography variant="body" level={2} color="default">
+            The World Foundation is launching a series of proposals to
+            practically explore and demonstrate the potential of
+            one-person-one-vote coordination. Each proposal will focus on a
+            unique topic ranging from community value alignment to helping the
+            Foundation shape priorities all while supporting a novel method of
+            engaging with the World community. Every verified user will receive
+            exactly one vote, backed by proof-of-human with World ID.
+          </Typography>
+          <Typography variant="body" level={2} color="default">
+            Leveraging first-class governance technology powered by Agora and
+            World ID, an anonymous proof-of-human mechanism, it ensures every
+            vote on World proposals is cast by a verified human, providing
+            sybil-resistant participation.
           </Typography>
         </div>
       </Page.Main>
